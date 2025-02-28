7b0a71af6ff45cd42ff83d14ca9a7347f025d074
manel652 <manelbenfarah01@gmail.com>
2025-02-28 10:03:42 +0100

    some changes

---
+++ src/pages/authentication/Login.tsx
@@ -0,0 +1,185 @@
+import { LoadingButton } from "@mui/lab";
+import {
+  Box,
+  Button,
+  Card,
+  FormHelperText
+} from "@mui/material";
+import {
+  TextFieldWrapper
+} from "components/authentication/StyledComponents";
+import FlexBox from "components/FlexBox";
+import LightTextField from "components/LightTextField";
+import { H1, Paragraph, Small } from "components/Typography";
+import { useFormik } from "formik";
+import useAuth from "hooks/useAuth";
+import { FC, useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+
+const Login: FC = () => {
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  let navigate = useNavigate();
+
+  const initialValues = {
+    email: "demo@example.com",
+    password: "v&)3?2]:",
+    submit: null,
+    remember: true,
+  };
+  // form field value validation schema
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Must be a valid email")
+      .max(255)
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password should be of minimum 6 characters length")
+      .required("Password is required"),
+  });
+
+  const { errors, values, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit: (values: any) => {
+        setLoading(true);
+        login(values.email, values.password)
+          .then(() => {
+            setLoading(false);
+            toast.success("You Logged In Successfully test");
+            navigate("/dashboard");
+          })
+          .catch((error) => {
+            setError(error.message);
+            setLoading(false);
+          });
+      },
+    });
+
+  return (
+    <FlexBox
+      sx={{
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: { sm: "100%" },
+      }}
+    >
+      <Card sx={{ padding: 4, maxWidth: 600, boxShadow: 1 }}>
+        <FlexBox
+          alignItems="center"
+          flexDirection="column"
+          justifyContent="center"
+          mb={5}
+        >
+          <Box
+            width={38}
+            mb={1}
+            display="flex" // Ensure Box behaves like a flex container
+            justifyContent="center" // Center the content horizontally
+            alignItems="center" // Center the content vertically if needed
+          >
+            <img
+              src="/static/logo/HB.jpg"
+              width="150px" // Set the desired width
+              height="auto"
+              alt="HB Logo"
+            />
+          </Box>
+          <H1 fontSize={24} color="#004687" fontWeight={700}>
+            Se Connecter
+          </H1>
+        </FlexBox>
+
+        <FlexBox justifyContent="space-between" flexWrap="wrap" my="1rem">
+
+
+
+
+
+          <form noValidate onSubmit={handleSubmit} style={{ width: "100%" }}>
+            <FlexBox justifyContent="space-between" flexWrap="wrap">
+              <TextFieldWrapper>
+                <Paragraph fontWeight={600} mb={1}>
+                  Email
+                </Paragraph>
+                <LightTextField
+                  fullWidth
+                  name="email"
+                  type="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email || ""}
+                  error={Boolean(touched.email && errors.email)}
+                  helperText={touched.email && errors.email ? String(errors.email) : ""}
+                />
+              </TextFieldWrapper>
+
+              <TextFieldWrapper>
+                <Paragraph fontWeight={600} mb={1}>
+                  Mot de passe
+                </Paragraph>
+                <LightTextField
+                  fullWidth
+                  name="password"
+                  type="password"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.password || ""}
+                  error={Boolean(touched.password && errors.password)}
+                  helperText={touched.email && errors.email ? String(errors.email) : ""}
+                />
+              </TextFieldWrapper>
+            </FlexBox>
+
+            <FlexBox mt={2} alignItems="center" justifyContent="space-between">
+
+              <Link to="/forget-password">
+                <Small color="secondary.red">Mot de passe oublié?</Small>
+              </Link>
+            </FlexBox>
+
+            {error && (
+              <FormHelperText
+                error
+                sx={{
+                  mt: 2,
+                  fontSize: 13,
+                  fontWeight: 500,
+                  textAlign: "center",
+                }}
+              >
+                {error}
+              </FormHelperText>
+            )}
+
+            <Box sx={{ mt: 4 }}>
+              {loading ? (
+                <LoadingButton loading fullWidth variant="contained">
+                  Se Connecter
+                </LoadingButton>
+              ) : (
+                <Button style={{ backgroundColor: "#004687" }} fullWidth type="submit" variant="contained">
+                  Se Connecter
+                </Button>
+              )}
+            </Box>
+          </form>
+
+          <Small margin="auto" mt={3} color="text.disabled">
+            Vous n'avez pas de compte ? {" "}
+            <Link to="/register">
+              <Small color="primary.main">Créer un compte?</Small>
+            </Link>
+          </Small>
+        </FlexBox>
+      </Card>
+    </FlexBox>
+  );
+};
+
+export default Login;
---
+++ src/pages/authentication/Register.tsx
@@ -0,0 +1,187 @@
+import { LoadingButton } from "@mui/lab";
+import {
+  Box,
+  Button,
+  Card,
+  FormHelperText
+} from "@mui/material";
+import {
+  TextFieldWrapper
+} from "components/authentication/StyledComponents";
+import FlexBox from "components/FlexBox";
+import LightTextField from "components/LightTextField";
+import { Small } from "components/Typography";
+import { useFormik } from "formik";
+import useAuth from "hooks/useAuth";
+import { FC, useState } from "react";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+
+const Register: FC = () => {
+  const { register } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+    terms: true,
+    submit: null,
+  };
+  // form field value validation schema
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string()
+      .email("Must be a valid email")
+      .max(255)
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password should be of minimum 6 characters length")
+      .required("Password is required"),
+  });
+
+  const { errors, values, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit: async (values: any) => {
+        setLoading(true);
+        try {
+          await register(values.email, values.password, values.name);
+          setLoading(false);
+          toast.success("You registered successfully");
+          navigate("/dashboard");
+        } catch (error: any) {
+          setError(error?.message);
+          setLoading(false);
+        }
+      },
+    });
+
+  return (
+    <FlexBox
+      sx={{
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: { sm: "100%" },
+      }}
+    >
+      <Card sx={{ padding: 4, maxWidth: 600, boxShadow: 1 }}>
+        <FlexBox
+          alignItems="center"
+          flexDirection="column"
+          justifyContent="center"
+          mb={5}
+        >
+          <Box
+            width={38}
+            mb={1}
+            display="flex" // Ensure Box behaves like a flex container
+            justifyContent="center" // Center the content horizontally
+            alignItems="center" // Center the content vertically if needed
+          >
+            <img
+              src="/static/logo/HB.jpg"
+              width="150px" // Set the desired width
+              height="auto"
+              alt="Hopital Hbib Bourgiba"
+            />
+          </Box>
+        </FlexBox>
+
+        <FlexBox justifyContent="space-between" flexWrap="wrap" my="1rem">
+
+
+          <form noValidate onSubmit={handleSubmit} style={{ width: "100%" }}>
+            <FlexBox justifyContent="space-between" flexWrap="wrap">
+              <TextFieldWrapper>
+                <LightTextField
+                  fullWidth
+                  name="name"
+                  type="text"
+                  label="Nom"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.name || ""}
+                  error={Boolean(touched.name && errors.name)}
+                  helperText={touched.email && errors.email ? String(errors.email) : ""}
+                />
+              </TextFieldWrapper>
+
+              <TextFieldWrapper>
+                <LightTextField
+                  fullWidth
+                  name="email"
+                  type="email"
+                  label="Email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.email || ""}
+                  error={Boolean(touched.email && errors.email)}
+                  helperText={touched.email && errors.email ? String(errors.email) : ""}
+                />
+              </TextFieldWrapper>
+            </FlexBox>
+
+            <TextFieldWrapper sx={{ mt: 2, width: "100%" }}>
+              <LightTextField
+                fullWidth
+                name="password"
+                type="password"
+                label="mot de passe"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password || ""}
+                error={Boolean(touched.password && errors.password)}
+                helperText={touched.email && errors.email ? String(errors.email) : ""}
+              />
+            </TextFieldWrapper>
+
+
+
+            {error && (
+              <FormHelperText
+                error
+                sx={{
+                  mt: 2,
+                  fontSize: 13,
+                  fontWeight: 500,
+                  textAlign: "center",
+                }}
+              >
+                {error}
+              </FormHelperText>
+            )}
+
+            <Box sx={{ mt: 4 }}>
+              {loading ? (
+                <LoadingButton loading fullWidth variant="contained">
+
+                  S'inscrire
+                </LoadingButton>
+              ) : (
+                <Button fullWidth type="submit" variant="contained">
+
+                  S'inscrire
+                </Button>
+              )}
+            </Box>
+          </form>
+
+          <Small margin="auto" mt={3} color="text.disabled">
+            Avez-vous déjà un compte ? {" "}
+            <Link to="/login">
+              <Small color="primary.main">Se connecter?</Small>
+            </Link>
+          </Small>
+        </FlexBox>
+      </Card>
+    </FlexBox>
+  );
+};
+
+export default Register;
